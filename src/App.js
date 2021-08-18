e29c91e88c33b4d3974783bbef9e0d82a68204b1
Siddharth Singh <iamsidz89@gmail.com>
2021-08-18 11:07:10 +0530

    first component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@
 // dev tools me sources me whole react dom code and package code , not just our code , there will be app function not same tough
 
 import ExpenseItem from "./components/ExpenseItems";
+const expenses = [
+  { title: " Spalding Basketball", amount: 1999, date: new Date(2021, 7, 18) },
+  { title: "Ryzen7", amount: 29000, date: new Date(2021, 7, 10) },
+  { title: "Mouse", amount: 499, date: new Date(2021, 7, 9) },
+];
 function App() {
   return (
     <div className="App">
-     <h1>lets go man</h1>
-     <p>This is also in app.js</p>
-     {/* if it would have js , then create paragraph append etc.... */}
-     <ExpenseItem></ExpenseItem>
+      <h1>lets go man</h1>
+      <p>This is also in app.js</p>
+      {/* if it would have js , then create paragraph append etc.... */}
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,16 +1,22 @@
 // a component is just a js function
 import './ExpenseItem.css' ;
-function ExpenseItem() {
+function ExpenseItem(props) {   //one parameter as object and will store all the properties hence props
+// month satrts from 0
+    // const edate = new Date(2021,7,18);
+    // const etitle = 'Spalding Basketball';
+    // const eamount = '900INR' 
+    // should be in app.js no here so that the data is independent
     return (
         // instead of class using className
         <div className='expense-item'>   
-            <div>DATE</div>
+            <div>{props.date.toISOString()}</div> {/*to convert in string the date value use isostring */}
             <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <h2>{props.title}</h2>
+                {/* inside {} can run basic js .. */}
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
 }
-
+// data is passed via props
 export default ExpenseItem;
