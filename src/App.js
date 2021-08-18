fd209a0c5b5401a49b383e65b792c99c26da748d
Siddharth Singh <iamsidz89@gmail.com>
2021-08-18 13:52:55 +0530

    assignment1 done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@
 //  here is html code inside js file ---- this is JSX[javascript XML]
 // dev tools me sources me whole react dom code and package code , not just our code , there will be app function not same tough
 
-import ExpenseItem from "./components/ExpenseItems";
+// import ExpenseItem from "./components/ExpenseItems";
+import Expenses from './components/Expenses'
 const expenses = [
   { title: " Spalding Basketball", amount: 1999, date: new Date(2021, 7, 18) },
   { title: "Ryzen7", amount: 29000, date: new Date(2021, 7, 10) },
@@ -14,22 +15,9 @@ function App() {
     <div className="App">
       <h1>lets go man</h1>
       <p>This is also in app.js</p>
+      <Expenses expenses={expenses} />
       {/* if it would have js , then create paragraph append etc.... */}
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
+    
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,24 @@
+import ExpenseItem from "./ExpenseItems";
+function Expenses(props) {
+  return (
+    <div>
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
